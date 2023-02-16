b7b4af283057add9e2a2b0ae961e9768376acfdc
Dinara <dineirashafigulllina@gmail.com>
2023-02-16 11:31:16 +0500

    hide key

--- parser_tg/settings.py
+++ parser_tg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from core.config_api import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q9#4_v3)56wx27%^-apvn^8azae@=lu&*t=!qs38+hafac(h3g'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
